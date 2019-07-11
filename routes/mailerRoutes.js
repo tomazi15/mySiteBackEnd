d3cefec5ffdffe085f059ad4225573810eef419f
tomasz <tomazi.dev@gmail.com>
2019-07-11 11:29:07 +0100

    adding corse to the response header

--- routes/mailerRoutes.js
+++ routes/mailerRoutes.js
@@ -1,8 +1,14 @@
 var mailer = require('../controllers/mailerController');
 const troll = new mailer();
+const cors = require('cors');
+
+// For future when FE deployed
+const corsOptions = {
+    origin: 'https://yourdomain.com'
+  }
 
 const routes = ((app) => {
-    app.route('/api/mailer')
+    app.route('/api/mailer', cors())
         .post((req, res) => {
             troll.mailer(req, res);
         })
